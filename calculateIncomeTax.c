f350a1fe24d06cbeeb5249bc2f716c367026ed57
YashAI060 <yt888885@gmail.com>
2025-01-20 18:27:32 +0500

    calculateIncomeTax.c

--- calculateIncomeTax.c
+++ calculateIncomeTax.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 
-void showLoadSheddingSchedule(char city[]) {
-    printf("Load Shedding Schedule for %s:\n", city);
-    
-    printf("Monday: 6 AM - 8 AM, 6 PM - 8 PM\n");
-    printf("Tuesday: 7 AM - 9 AM, 5 PM - 7 PM\n");
-    printf("Wednesday: 6 AM - 8 AM, 6 PM - 8 PM\n");
-    printf("Thursday: 7 AM - 9 AM, 5 PM - 7 PM\n");
-    printf("Friday: 6 AM - 8 AM, 6 PM - 8 PM\n");
-    printf("Saturday: 7 AM - 9 AM, 5 PM - 7 PM\n");
-    printf("Sunday: 6 AM - 8 AM, 6 PM - 8 PM\n");
+float calculateIncomeTax(float salary) {
+    float annualSalary = salary * 12;
+    float tax = 0.0;
+
+    if (annualSalary < 600000) {
+        tax = 0.0;
+    } else if (annualSalary <= 1200000) {
+        tax = (annualSalary - 600000) * 0.025;
+    } else if (annualSalary <= 2400000) {
+        tax = (600000 * 0.025) + (annualSalary - 1200000) * 0.125;
+    } else if (annualSalary <= 3600000) {
+        tax = (600000 * 0.025) + (1200000 * 0.125) + (annualSalary - 2400000) * 0.20;
+    } else {
+        tax = (600000 * 0.025) + (1200000 * 0.125) + (1200000 * 0.20) + (annualSalary - 3600000) * 0.30;
+    }
+
+    return tax;
 }
 
 int main() {
-    char city[50];
+    float monthlySalary;
 
-    printf("Enter your city name: ");
-    scanf("%49s", city);
+    printf("Enter your monthly salary (in PKR): ");
+    scanf("%f", &monthlySalary);
     
-    showLoadSheddingSchedule(city);
+    float annualTax = calculateIncomeTax(monthlySalary);
+    
+    printf("Annual Tax = %.2f PKR\n", annualTax);
     
     return 0;
 }
